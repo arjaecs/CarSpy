ce36f578e7423ac9ca9f295b116b33fd4084294a
Alvaro Calderon <arjaecs@gmail.com>
2013-03-04 00:59:06 -0400

    Redesign

--- checkAuth.php
+++
@@ -1,8 +0,0 @@
-<?php       // Verifies if user has a cookie set to identify if he has a session open
-
-if(isset($_COOKIE['loggedin'])){
-	$loggedin = true;
-	$id = $_COOKIE['loggedin'];
-}
-
-?>
--- logoutHandler.php
+++
@@ -1,8 +0,0 @@
-<?php       // logsout of the account depending if user is in mobile or on desktop version
-
-if(isset($_POST['loggedOut'])  &&  $_POST['loggedOut'] == 'logout'){
-	setcookie('loggedin', false);
-	header('Location: index.php');
-	return;
-}
-?>
